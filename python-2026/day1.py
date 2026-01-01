e7e9863e5f3a1dcd3236481c84091b4b6b9f5553
Ankit Ojha <ankitojha1409@gmail.com>
2026-01-01 22:48:01 +0530

    functions and if else

--- python-2026/day1.py
+++ python-2026/day1.py
@@ -1,8 +1,10 @@
-def greet_user(name,experience_year):
-    if experience_year >=3:
-        return f"Welcom back, {name}! You're and experienced developer."
+def greet_user(name,experience_year, language):
+    if experience_year >=3 and language == "Python" or language == "py" or language == "python":
+        return f"Welcom back PyDev, {name}! You're and experienced {language} developer."
+    if experience_year >=3 and language == "JavaScript" or language == "js" or language == "Javascript":
+        return f"Welcom back JS Dev, {name}! You're and experienced {language} developer."
     else:
         return f"Hi, {name}! Let's level up your skills."
 
-message = greet_user("Ankit", 3)
+message = greet_user("Ankit", 3, "js")
 print(message)
